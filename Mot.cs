29eb6599398621c4f886308d29031c608cfdd3d5
gabrielsaidomar <gabrielsaidomar@GABRIELSAID60A5>
2020-10-21 14:38:39 +0200

    function du mot

---
+++ Mot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Project2
+{
+    class Mot
+    {
+
+        public static void mot()
+        {
+            string password = "";
+            Console.Write("Entre un mot en français : ");
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                password += keyInfo.KeyChar;
+                Console.Write("*");
+            }
+            while (keyInfo.Key != ConsoleKey.Enter);
+        }
+      
+      
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,14 @@
-﻿using System;
+﻿using Project2;
+using System;
 using System.Collections.Generic;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        //Console.WriteLine("Password Masking Console Application");
-        //Console.WriteLine("------------------------------------");
-        //Console.Write("Enter username: ");
-        //string username = Console.ReadLine();
-        string password = "";
-        Console.Write("Enter password: ");
-        ConsoleKeyInfo keyInfo;
-
-
-        do
-        {
-            keyInfo = Console.ReadKey(true);
-            // Skip if Backspace or Enter is Pressed
-            //if (keyInfo.Key != ConsoleKey.Backspace && keyInfo.Key != ConsoleKey.Enter)
-            //{
-                password += keyInfo.KeyChar;
-                Console.Write("*");
-            //}
-            //else
-            //{
-            //    if (keyInfo.Key == ConsoleKey.Backspace && password.Length > 0)
-            //    {
-            //        // Remove last charcter if Backspace is Pressed
-            //        password = password.Substring(0, (password.Length - 1));
-            //        Console.Write("b b");
-            //    }
-            //}
-        }
-        // Stops Getting Password Once Enter is Pressed
-        while (keyInfo.Key != ConsoleKey.Enter);
-////////////comentaire ////////////////////
-        //Console.WriteLine();
-        //Console.WriteLine("---------------------------");
-        //Console.WriteLine("Welcome " + username + ",");
-        //Console.WriteLine("Your Password is : " + password);
+        Mot.mot();
 
     }
+
+    
 }
