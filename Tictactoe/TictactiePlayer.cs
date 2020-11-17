66839488c3ff87719b16500f4b3c03ca022d7299
fabien DESNOUES <fabien.desnoues31@gmail.com>
2020-11-17 14:37:00 +0100

    create branch Player

---
+++ Tictactoe/TictactiePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Player
+{
+	private string Name;
+
+	public Player(string Player1)
+	{
+		Name = Player1;
+	}
+
+	public Player(string Player2)
+	{
+		Name = Player2;
+	}
+	public string ChooseName()
+    {
+		Console.ReadLine(Name);
+	}
+}
